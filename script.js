e172bcca514ccb3bd1e1cbdf0d2850730459f9e5
Bradleyh212 <bdesm052@uottawa.ca>
2025-05-25 23:29:18 -0400

    added the comment for my info in each code

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-window.addEventListener("scroll", function () {
-  const top_navbar = document.getElementById("top_navbar");
-  const side_navbar = document.getElementById("side_navbar");
+/*
+  Author: Bradley-Hans Desmornes
+  GitHub: https://github.com/bradleydesmornes  # or your LinkedIn
+  Date: 2025-05-24
+  Description: JS script for my portfolio 
+*/
 
-  if (window.scrollY > 100) {
-    top_navbar.style.display = "none";
-    side_navbar.style.display = "flex";
-  } else {
-    top_navbar.style.display = "flex";
-    side_navbar.style.display = "none";
-  }
-});
+// window.addEventListener("scroll", function () {
+//   const top_navbar = document.getElementById("top_navbar");
+//   const side_navbar = document.getElementById("side_navbar");
+
+//   if (window.scrollY > 100) {
+//     top_navbar.style.display = "none";
+//     side_navbar.style.display = "flex";
+//   } else {
+//     top_navbar.style.display = "flex";
+//     side_navbar.style.display = "none";
+//   }
+// });
