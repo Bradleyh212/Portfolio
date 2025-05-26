06bdfaaa80b416d0055936c637297e9e6bb44d94
Bradleyh212 <bdesm052@uottawa.ca>
2025-05-25 22:22:06 -0400

    almost done the main page

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-script.js
+window.addEventListener("scroll", function () {
+  const topNavbar = document.getElementById("top_navbar");
+  const sidebarNav = document.getElementById("side_navbar");
+
+  if (window.scrollY > 100) {
+    topNavbar.style.display = "none";
+    sidebarNav.style.display = "flex";
+  } else {
+    topNavbar.style.display = "flex";
+    sidebarNav.style.display = "none";
+  }
+});
