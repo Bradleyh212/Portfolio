67a598c1ea6e6e4835636086ed3f8b6dd79f4530
Bradleyh212 <bdesm052@uottawa.ca>
2025-05-26 19:52:20 -0400

    removed the side nav bar for now, if my portfolio becomes bigger later on i will add it back

--- script.js
+++ script.js
@@ -5,15 +5,3 @@
   Description: JS script for my portfolio 
 */
 
-// window.addEventListener("scroll", function () {
-//   const top_navbar = document.getElementById("top_navbar");
-//   const side_navbar = document.getElementById("side_navbar");
-
-//   if (window.scrollY > 100) {
-//     top_navbar.style.display = "none";
-//     side_navbar.style.display = "flex";
-//   } else {
-//     top_navbar.style.display = "flex";
-//     side_navbar.style.display = "none";
-//   }
-// });
